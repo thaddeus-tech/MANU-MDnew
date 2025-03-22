115b846c02a68fabc40384d32a566fa0500faffb
thaddeus-tech <mmoffat687@gmail.com>
2025-03-22 15:56:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,36 +38,40 @@
 
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault;
 }
+
 module.exports = {
-    SESSION_ID:process.env.SESSION_ID || 'Manu-MD&hPVxATwB#8RjSQXmqNSHTC_qMo3wfpc4aU0xQmzxEPx27xZrS7WI',
+    SESSION_ID: process.env.SESSION_ID || 'Manu-MD&hPVxATwB#8RjSQXmqNSHTC_qMo3wfpc4aU0xQmzxEPx27xZrS7WI',
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'thaddeus-tech/Manu-XMD',
-    GITHUB_AUTH_TOKEN : process.env.GITHUB_AUTH_TOKEN || '4Au6FwBxZPaY2iGc3Ky1V4yd9qh2Hb4CZfo1',
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '4Au6FwBxZPaY2iGc3Ky1V4yd9qh2Hb4CZfo1',
     getSessionDetails: () => {
         return `Session ID: ${process.env.SESSION_ID}`;
-PREFIX: process.env.PREFIX || '.' ,
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SUDO: process.env.SUDO === undefined ? '254740075968' : process.env.SUDO,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
-MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
-AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
-ANTI_BAD: process.env.ANTI_BAD === undefined ? "false" : process.env.ANTI_BAD,
-ANTI_LINK: process.env.ANTI_LINK === undefined ? "true" : process.env.ANTI_LINK,
-ANTI_CALL: process.env.ANTI_CALL === undefined ? "true" : process.env.ANTI_CALL,    
-DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
-ANTI_DELETE : process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE,
-ANTI_BOT: process.env.ANTI_BOT === undefined ? "true" : process.env.ANTI_BOT,
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE === undefined ? "false" : process.env.ALWAYS_ONLINE,
-READ_CMD: process.env.READ_CMD === undefined ? "true" : process.env.READ_CMD,
-RECORDING: process.env.RECORDING === undefined ? "true" : process.env.RECORDING,
-AI_CHAT: process.env.AI_CHAT === undefined ? "false" : process.env.AI_CHAT,
-AUTO_REACT: process.env.AUTO_REACT === undefined ? "false" : process.env.AUTO_REACT,
-AUTO_TYPING: process.env.AUTO_TYPING === undefined ? 'false' : process.env.AUTO_TYPING,
-AUTO_BIO: process.env.AUTO_BIO === undefined ? 'true' : process.env.AUTO_BIO,
-ANTI_STICKER: process.env.ANTI_STICKER === undefined ? 'false' : process.env.ANTI_STICKER,
-ANTI_PHOTO: process.env.ANTI_PHOTO === undefined ? 'false' : process.env.ANTI_PHOTO,
-MOROCCO_BLOCK: process.env.MOROCCO_BLOCK === undefined ? 'false' : process.env.MOROCCO_BLOCK,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'Manu-MD&hPVxATwB#8RjSQXmqNSHTC_qMo3wfpc4aU0xQmzxEPx27xZrS7WI' : process.env.POSTGRESQL_URL,
+    },
+    PREFIX: process.env.PREFIX || '.',
+    PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+    SUDO: process.env.SUDO === undefined ? '254740075968' : process.env.SUDO,
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? "true" : process.env.AUTO_READ_STATUS,
+    MODE: process.env.MODE === undefined ? "public" : process.env.MODE,
+    AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
+    ANTI_BAD: process.env.ANTI_BAD === undefined ? "false" : process.env.ANTI_BAD,
+    ANTI_LINK: process.env.ANTI_LINK === undefined ? "true" : process.env.ANTI_LINK,
+    ANTI_CALL: process.env.ANTI_CALL === undefined ? "true" : process.env.ANTI_CALL,
+    DELETEMSGSENDTO: process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
+    ANTI_DELETE: process.env.ANTI_DELETE === undefined ? "true" : process.env.ANTI_DELETE,
+    ANTI_BOT: process.env.ANTI_BOT === undefined ? "true" : process.env.ANTI_BOT,
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE === undefined ? "false" : process.env.ALWAYS_ONLINE,
+    READ_CMD: process.env.READ_CMD === undefined ? "true" : process.env.READ_CMD,
+    RECORDING: process.env.RECORDING === undefined ? "true" : process.env.RECORDING,
+    AI_CHAT: process.env.AI_CHAT === undefined ? "false" : process.env.AI_CHAT,
+    AUTO_REACT: process.env.AUTO_REACT === undefined ? "false" : process.env.AUTO_REACT,
+    AUTO_TYPING: process.env.AUTO_TYPING === undefined ? 'false' : process.env.AUTO_TYPING,
+    AUTO_BIO: process.env.AUTO_BIO === undefined ? 'true' : process.env.AUTO_BIO,
+    ANTI_STICKER: process.env.ANTI_STICKER === undefined ? 'false' : process.env.ANTI_STICKER,
+    ANTI_PHOTO: process.env.ANTI_PHOTO === undefined ? 'false' : process.env.ANTI_PHOTO,
+    MOROCCO_BLOCK: process.env.MOROCCO_BLOCK === undefined ? 'false' : process.env.MOROCCO_BLOCK,
+    POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'Manu-MD&hPVxATwB#8RjSQXmqNSHTC_qMo3wfpc4aU0xQmzxEPx27xZrS7WI' : process.env.POSTGRESQL_URL,
 };
+
